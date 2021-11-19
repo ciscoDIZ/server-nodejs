917cceb9661e51a5f69ed16faf6362f216c63089
tote <ciscoadiz@gmail.com>
2021-11-19 17:01:51 +0000

    cabeceras v0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
+
     if (req.method === 'GET') {
-        res.write('<h1>Mi primer servidor en Node.js</h1>');
+        res.writeHead(200, {'Content-type': 'application/json; charset=UTF-8'})
+        res.write(`{"status":${res.statusCode},"message":"Mi primer servidor en Node.js"}`);
         return res.end();
     }
+    res.writeHead(405, {'Content-type': 'text/html; charset=UTF-8'})
     res.write('<h1>Método no permitido</h1>');
     return res.end();
 
