a4beaf5f512d7d0b53b0511f84c70461010eb6a2
tote <ciscoadiz@gmail.com>
2021-11-22 08:25:38 +0000

    home vitaminado v0.2

--- src/index.js
+++ src/index.js
@@ -1,46 +1,41 @@
 import http from 'http';
 import fs from 'fs';
+import path from "path";
 
-/*const home = './src/index.html';
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset:UTF-8'});
-    fs.readFile(home, (err, content) => {
-        if (err) return console.log(err.toString());
-        res.write(content);
-        res.end();
-    })
-});
-
-server.listen(8080, '0.0.0.0', (err) => {
-    if (err) console.log(`Error: ${err.toString()}`);
-    const {address} = server.address();
-    console.log(`conexión abierta en http://${address}:8080`);
-})*/
-const home = `./src/html/index.html`
-const notAllowedMethod = './src/html/error/not-allowed-method/index.html'
 const server = http.createServer((request, response) => {
-    if (request.method === 'GET') {
-        response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
-        fs.readFile(home, (err, content) => {
-            if (err) {
-                return console.log(err)
-            }
-            response.write(content)
-            return response.end()
-        })
-    }
-    response.writeHead(403, { 'Content-Type': 'text/html; charset=UTF-8' })
-    fs.readFile(notAllowedMethod, (err, content) => {
-        if(err) {
-            return console.log(err);
-        }
-        response.write(content)
-        return response.end()
-    })
+   let filePath = request.url;
+   if (filePath === '/') {
+       filePath = 'index.html';
+   }
+
+   const extname = path.extname(filePath);
+
+   let contentType = '';
+   switch (extname) {
+       case '.css':
+           contentType = 'text/css';
+           filePath = '.'+filePath;
+           break;
+       case '.html':
+           contentType = 'text/html';
+           filePath = `./src/assets/html/${filePath}`;
+           break;
+       case '.ico':
+           contentType = 'image/x-ico';
+           filePath = '.'+filePath;
+           break;
+
+   }
+    response.writeHead(200, {'Content-Type': `${contentType}; charset=UTF-8`});
+   fs.readFile(filePath, (err, content) => {
+      if(err) return console.log(err);
+      response.write(content);
+      response.end();
+   });
 })
 
-server.listen(8080, 'localhost', err => {
+server.listen(8081, '0.0.0.0', err => {
     if (err) {
         return console.log('Error: ', err)
     }
