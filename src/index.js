4dee0ed2d56c218b7b8a913dc51b9d4ccbe71599
tote <ciscoadiz@gmail.com>
2021-11-24 12:14:04 +0000

    configurando express v0.2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from "body-parser";
 
 const app = express();
+app.disable('x-powered-by');
+app.set('env', 'development');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: 'force'}))
 
 app.get('/', (req, res) => {
     res.end('hello world');
